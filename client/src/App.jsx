98167f966815de9fe5fba3edf6bc614797e30bcf
imanihenderson <imanijh15@gmail.com>
2025-09-29 11:29:27 -0400

    Frontend files

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import './App.css'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+  
+    <div>
+
+    </div>
+  )
+}
+
+export default App;
